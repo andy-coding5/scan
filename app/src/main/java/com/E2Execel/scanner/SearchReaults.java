5665c0cb6af7ca28452b357a39c80bfe357af0f8
andy-coding5 <andy.coding852@gmail.com>
2019-02-08 10:51:58 +0530

    back button replaced

--- app/src/main/java/com/E2Execel/scanner/SearchReaults.java
+++ app/src/main/java/com/E2Execel/scanner/SearchReaults.java
@@ -5,6 +5,9 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.E2Execel.scanner.Pojo.search_details.Datum;
@@ -18,10 +21,19 @@ public class SearchReaults extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_reaults);
+
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setDisplayShowCustomEnabled(true);
-        assert getSupportActionBar() != null;   //null check
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);   //show back button
+        getSupportActionBar().setCustomView(R.layout.custom_action_bar_layout);
+        View view = getSupportActionBar().getCustomView();
+
+        ImageButton imageButton = (ImageButton) view.findViewById(R.id.action_bar_back);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
 
         //get intent from main activity
@@ -29,13 +41,7 @@ public class SearchReaults extends AppCompatActivity {
         ArrayList list = (ArrayList<Datum>) i.getSerializableExtra("datum_list");
         Toast.makeText(this, "list successfully", Toast.LENGTH_SHORT).show();
 
-
-
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        finish();
-        return true;
-    }
+
 }
